332d4ac267363f47a6265340f37970ad13abe024
jakedahn <jake@shruggingface.com>
2024-12-29 15:38:33 -0700

    enabling os_mon

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Clickthebutton.MixProject do
   def application do
     [
       mod: {Clickthebutton.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
